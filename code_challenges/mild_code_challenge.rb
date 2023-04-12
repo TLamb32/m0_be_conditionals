a96cc8353d8373178922994def19a91ac5b7a13a
Terry Lamb <terrylamb@gmail.com>
2023-04-12 15:31:24 -0400

    Solve the mild coding challenge

---
+++ code_challenges/mild_code_challenge.rb
@@ -0,0 +1,11 @@
+# Mild
+
+# Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+
+age = 8
+
+if age.even?
+    puts "even"
+elsif !age.even?
+    puts "odd"
+end
